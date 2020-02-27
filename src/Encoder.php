4df3a23a9868f95c35f5ba6fa7d85fe77c53317d
Carlos Afonso Perez <cafonsop@amazon.com>
2020-02-27 22:43:49 +0100

    Add initial working implementation

---
+++ src/Encoder.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Afonso\Base24;
+
+class Encoder
+{
+	const ALPHABET = 'ZAC2B3EF4GH5TK67P8RS9WXY';
+	const ALPHABET_LENGTH = 24;
+
+	private $encodeMap = [];
+	private $decodeMap = [];
+
+	public function __construct()
+	{
+		for ($i = 0; $i < strlen(self::ALPHABET); $i++)
+		{
+			$this->decodeMap[self::ALPHABET[$i]] = $i;
+			$this->decodeMap[strtolower(self::ALPHABET[$i])] = $i;
+			$this->encodeMap[$i] = self::ALPHABET[$i];
+		}
+	}
+
+	public function encode(array $str): string
+	{
+		$dataLength = count($str);
+
+		if ($dataLength % 4 !== 0)
+		{
+			throw new \InvalidArgumentException('Input to encode must have a length multiple of 4');
+		}
+
+		$result = '';
+		for ($i = 0; $i < $dataLength / 4; $i++)
+		{
+			$j = $i * 4;
+			$mask = 0xFF;
+
+			$b3 = $str[$j] & $mask;
+			$b2 = $str[$j + 1] & $mask;
+			$b1 = $str[$j + 2] & $mask;
+			$b0 = $str[$j + 3] & $mask;
+
+			$value = 0xFFFFFFFF &
+				(($b3 << 24) | ($b2 << 16) | ($b1 << 8) | $b0);
+
+			$subResult = '';
+			for ($k = 0; $k < 7; $k++)
+			{
+				$idx = $value % 24;
+				$value = $value / self::ALPHABET_LENGTH;
+				$subResult = $this->encodeMap[$idx] . $subResult;
+			}
+
+			$result .= $subResult;
+		}
+		return $result;
+	}
+
+	public function decode(string $str): array
+	{
+		$dataLength = strlen($str);
+
+		if ($dataLength % 7 !== 0)
+		{
+			throw new \InvalidArgumentException('Input to decode must have a length multiple of 7');
+		}
+
+		$bytes = [];
+		for ($i = 0; $i < $dataLength / 7; $i++) {
+			$j = $i * 7;
+			$subData = substr($str, $j, 7);
+			$value = 0;
+
+			foreach (str_split($subData) as $s)
+			{
+				$idx = $this->decodeMap[$s];
+				$value = self::ALPHABET_LENGTH * $value + $idx;
+			}
+
+			$mask = 0xFF;
+			$b0 = ($value & ($mask << 24)) >> 24;
+			$b1 = ($value & ($mask << 16)) >> 16;
+			$b2 = ($value & ($mask << 8)) >> 8;
+			$b3 = $value & $mask;
+
+			$bytes[] = $b0;
+			$bytes[] = $b1;
+			$bytes[] = $b2;
+			$bytes[] = $b3;
+		}
+
+		return $bytes;
+	}
+}
---
+++ tests/EncoderTest.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Afonso\Base24;
+
+class EncoderTest extends \PHPUnit\Framework\TestCase
+{
+	private $encoder;
+
+	protected function setUp(): void
+	{
+		$this->encoder = new Encoder();
+	}
+
+	/**
+	 * @dataProvider getTestData
+	 */
+	public function testEncode($decoded, $encoded): void
+	{
+		$this->assertSame(strtoupper($encoded), $this->encoder->encode($decoded));
+	}
+
+	/**
+	 * @dataProvider getTestData
+	 */
+	public function testDecode($decoded, $encoded): void
+	{
+		$this->assertSame($decoded, $this->encoder->decode($encoded));
+	}
+
+	public function testEncodeThrowsExceptionIfInputLengthIsNotMultipleOf4(): void
+	{
+		$this->expectException(\InvalidArgumentException::class);
+		$this->expectExceptionMessage('Input to encode must have a length multiple of 4');
+		$this->encoder->encode([0]);
+	}
+
+	public function testDecodeThrowsExceptionIfInputLengthIsNotMultipleOf7(): void
+	{
+		$this->expectException(\InvalidArgumentException::class);
+		$this->expectExceptionMessage('Input to decode must have a length multiple of 7');
+		$this->encoder->decode('A');
+	}
+
+	public function getTestData(): array
+	{
+		$mappings = [
+		    ['00000000', 'ZZZZZZZ'],
+		    ['000000000000000000000000', 'ZZZZZZZZZZZZZZZZZZZZZ'],
+		    ['00000001', 'ZZZZZZA'],
+		    ['000000010000000100000001', 'ZZZZZZAZZZZZZAZZZZZZA'],
+		    ['00000010', 'ZZZZZZP'],
+		    ['00000030', 'ZZZZZCZ'],
+		    ['88553311', '5YEATXA'],
+		    ['FFFFFFFF', 'X5GGBH7'],
+		    ['FFFFFFFFFFFFFFFFFFFFFFFF', 'X5GGBH7X5GGBH7X5GGBH7'],
+		    ['FFFFFFFFFFFFFFFFFFFFFFFF', 'x5ggbh7x5ggbh7x5ggbh7'],
+		    ['1234567887654321', 'A64KHWZ5WEPAGG'],
+		    ['1234567887654321', 'a64khwz5wepagg'],
+		    ['FF0001FF001101FF01023399', 'XGES63FZZ247C7ZC2ZA6G'],
+		    ['FF0001FF001101FF01023399', 'xges63fzz247c7zc2za6g'],
+		    ['25896984125478546598563251452658', '2FC28KTA66WRST4XAHRRCF237S8Z'],
+		    ['25896984125478546598563251452658', '2fc28kta66wrst4xahrrcf237s8z']
+		];
+
+		$testDarta = [];
+
+		foreach ($mappings as $mapping) {
+			$decoded = $mapping[0];
+			$encoded = $mapping[1];
+
+			$testData[] = [array_map('hexdec', str_split($decoded, 2)), $encoded];
+		}
+
+		return $testData;
+	}
+}
